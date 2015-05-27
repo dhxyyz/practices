0d8c6236ce9db4ddb7b473be06d6f16dc8c09ddd
dhxyyz <hd964660612@outlook.com>
2015-05-27 11:56:02 +0800

    practice_1

--- practices/filter/grouping_count.js
+++ practices/filter/grouping_count.js
@@ -1,9 +1,27 @@
 'use strict';
 
 function grouping_count(collection) {
-  var _ = require ('../lodash');
-  var result = [];
-  return _.xor(collection);
+  var decide = false;
+  var collection_a = [];
+  for (var i = 0; i < collection.length; i++) {
+    for (var j = 0; j < collection_a.length; j++) {
+      if (collection[i]===collection_a[j]) {
+        decide = true;
+      }
+    }
+    if (!decide) {
+      collection_a.push(collection[i]);
+    }
+    decide = false;
+  }
+
+  for ( i = 0; i < collection.length; i++) {
+
+  }
+
+  return collection_a;
+
+
 }
 
 module.exports = grouping_count;
--- practices/filter/two_collections_practice_1.js
+++ practices/filter/two_collections_practice_1.js
@@ -1,8 +1,17 @@
 'use strict';
 
 function choose_common_elements(collection_a, collection_b) {
-  var _ = require ('../lodash');
-  return _.intersection(collection_a, collection_b);
+//  var _ = require ('../lodash');
+//  return _.intersection(collection_a, collection_b);
+  var collection = [];
+  for (var i = 0; i < collection_a.length; i++) {
+    for (var j = 0; j < collection_b.length; j++) {
+      if (collection_a[i]===collection_b[j]) {
+        collection.push(collection_a[i]);
+      }
+    }
+  }
+  return collection;
 }
 
 module.exports = choose_common_elements;
