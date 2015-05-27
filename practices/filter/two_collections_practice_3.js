51c5a2354e17f1bd5eb0b3f1238c1eda9af54582
dhxyyz <hd964660612@outlook.com>
2015-05-27 14:44:58 +0800

    two3

--- practices/filter/two_collections_practice_3.js
+++ practices/filter/two_collections_practice_3.js
@@ -1,16 +1,20 @@
 'use strict';
 
 function choose_divisible_integer(collection_a, collection_b) {
-  var _ = require ('../lodash');
-
-  var a = [];
-
-  for (var i = 0; i < collection_b.length; i++) {
-    var evens = _.remove(collection_a, function(n) { return n % collection_b[i] === 0; });
-    a.push(evens);
+  var collection = [];
+  for (var i = 0; i < collection_a.length; i++) {
+    var test = false;
+    for (var j = 0; j < collection_b.length; j++) {
+      if (collection_a[i]%collection_b[j]===0) {
+        test = true;
+      }
+    }
+    if (test) {
+      collection.push(collection_a[i]);
+       test = false;
+    }
   }
-  var b = _.flatten(a);
-  return b;
+  return collection;
 }
 
 module.exports = choose_divisible_integer;
