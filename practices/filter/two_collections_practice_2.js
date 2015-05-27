9f53c3af5db535b529a7d1a43d64cb46c2259a61
dhxyyz <hd964660612@outlook.com>
2015-05-27 14:32:06 +0800

    two2

--- practices/filter/two_collections_practice_2.js
+++ practices/filter/two_collections_practice_2.js
@@ -1,8 +1,23 @@
 'use strict';
 
 function choose_no_common_elements(collection_a, collection_b) {
-  var _ = require ('../lodash');
-  return _.xor(collection_a, collection_b);
+//  var _ = require ('../lodash');
+//  return _.xor(collection_a, collection_b);
+  var collection = [];
+  for (var i = 0; i < collection_a.length; i++) {
+    var test = true;
+    for (var j = 0; j < collection_b.length; j++) {
+      if (collection_a[i]===collection_b[j]) {
+        test = false;
+        break;
+      }
+    }
+    if (test) {
+      collection.push(collection_a[i]);
+      test = true;
+    }
+  }
+  return collection;
 }
 
 module.exports = choose_no_common_elements;
