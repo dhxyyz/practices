bf620c08e8ce5049967c9190fa1bd6fc587136e2
dhxyyz <hd964660612@outlook.com>
2015-05-27 10:53:28 +0800

    git_integer_interval

--- practices/filter/choose_no_repeat_number.js
+++ practices/filter/choose_no_repeat_number.js
@@ -1,9 +1,21 @@
 'use strict';
 
 function choose_no_repeat_number(collection) {
-  var _ = require ('../lodash');
-  return _.union(collection);
+  //var _ = require ('../lodash');
+  //return _.union(collection);
+  var decide = true;
+  var collection_a = [];
+  for (var i = 0; i < collection.length; i++) {
+    for (var j = 0; j < collection_a.length; j++) {
+      if (collection[i]===collection_a[j]) {
+        break;
+      }
+    }
+    if (decide) {
+      collection_a.push(collection[i]);
+    }
+  }
+  return collection_a;
 }
 
 module.exports = choose_no_repeat_number;
-    
