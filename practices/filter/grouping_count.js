953277fd72eda9ee50459e85cc0c526ff9e7f1b8
dhxyyz <hd964660612@outlook.com>
2015-05-27 15:05:43 +0800

    grouping

--- practices/filter/grouping_count.js
+++ practices/filter/grouping_count.js
@@ -1,27 +1,13 @@
 'use strict';
 
 function grouping_count(collection) {
-  var decide = false;
-  var collection_a = [];
+  var array = {};
+
   for (var i = 0; i < collection.length; i++) {
-    for (var j = 0; j < collection_a.length; j++) {
-      if (collection[i]===collection_a[j]) {
-        decide = true;
-      }
-    }
-    if (!decide) {
-      collection_a.push(collection[i]);
-    }
-    decide = false;
+    array[collection[i]] = array[collection[i]] || 0;
+    array[collection[i]]++;
   }
-
-  for ( i = 0; i < collection.length; i++) {
-
-  }
-
-  return collection_a;
-
-
+  return array;
 }
 
 module.exports = grouping_count;
