aaa65d6bf714f9a8d4c95c117ffa815624a391ce
dhxyyz <hd964660612@outlook.com>
2015-05-28 12:07:29 +0800

    chain

--- practices/reduce/compute_chain_median.js
+++ practices/reduce/compute_chain_median.js
@@ -1,7 +1,33 @@
 'use strict';
 
 function compute_chain_median(collection) {
-  //在这里写入代码
+  var array = [];
+  array = collection.split('->');
+  for (var i = 0; i < array.length; i++) {
+    array[i] = parseInt(array[i]);
+  }
+
+  var number;
+  for (i = 0; i < array.length; i++) {
+    for (var j = i+1; j < array.length; j++) {
+      if (array[i]>array[j]) {
+        number=array[i];
+        array[i]=array[j];
+        array[j]=number;
+      }
+    }
+  }
+
+  var median;
+  var k;
+  if ((array.length)%2===0) {
+    k = (array.length)/2;
+    median=(array[k]+array[k-1])/2;
+  }else {
+    k = (array.length-1)/2;
+    median=array[k];
+  }
+  return median;
 }
 
 module.exports = compute_chain_median;
