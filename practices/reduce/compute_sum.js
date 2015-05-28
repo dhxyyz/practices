72bcb53cf0a0283bdb66e8892c91c1d66bd4c71b
dhxyyz <hd964660612@outlook.com>
2015-05-28 11:16:23 +0800

    sum

--- practices/reduce/compute_sum.js
+++ practices/reduce/compute_sum.js
@@ -1,8 +1,11 @@
 'use strict';
 
 function calculate_elements_sum(collection) {
-  //在这里写入代码
+  var sum = 0;
+  for (var i = 0; i < collection.length; i++) {
+    sum = sum + collection[i];
+  }
+  return sum;
 }
 
 module.exports = calculate_elements_sum;
-
