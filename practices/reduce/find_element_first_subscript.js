8d7226d0ae6903fb89a0c58a65d91e06b2f76d4a
dhxyyz <hd964660612@outlook.com>
2015-05-28 11:34:11 +0800

    element

--- practices/reduce/find_element_first_subscript.js
+++ practices/reduce/find_element_first_subscript.js
@@ -1,8 +1,14 @@
 'use strict';
 
 function calculate_elements_sum(collection, element) {
-  //在这里写入代码
+  var index;
+  for (var i = 0; i < collection.length; i++) {
+    if (element===collection[i]) {
+      index = i;
+      break;
+    }
+  }
+  return index;
 }
 
 module.exports = calculate_elements_sum;
-
