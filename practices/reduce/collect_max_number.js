14c2a65940067cd8c191df833aa0b0576b4bc223
dhxyyz <hd964660612@outlook.com>
2015-05-28 10:13:35 +0800

    max,min

--- practices/reduce/collect_max_number.js
+++ practices/reduce/collect_max_number.js
@@ -1,7 +1,16 @@
 'use strict';
 
 function collect_max_number(collection) {
-  //在这里写入代码
+  var number;
+  number = collection[1];
+  for (var i = 0; i < collection.length; i++) {
+    for (var j = i; j < collection.length; j++) {
+      if (number<collection[j]) {
+        number = collection[i];
+      }
+    }
+  }
+  return number;
 }
 
 module.exports = collect_max_number;
--- practices/reduce/collect_min_number.js
+++ practices/reduce/collect_min_number.js
@@ -1,8 +1,16 @@
 'use strict';
 
 function collect_min_number(collection) {
-  //在这里写入代码
+  var number;
+  number = collection[1];
+  for (var i = 0; i < collection.length; i++) {
+    for (var j = i; j < collection.length; j++) {
+      if (number>collection[j]) {
+        number = collection[i];
+      }
+    }
+  }
+  return number;
 }
 
 module.exports = collect_min_number;
-
