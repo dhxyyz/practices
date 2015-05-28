1cdda6609ac91a7c3d89602fa053659c9a834033
dhxyyz <hd964660612@outlook.com>
2015-05-28 17:39:56 +0800

    own

--- practices/superposition_operation/own_elements_operation/rank_by_two_large_one_small.js
+++ practices/superposition_operation/own_elements_operation/rank_by_two_large_one_small.js
@@ -1,5 +1,27 @@
 'use strict';
 function rank_by_two_large_one_small(collection){
-  //这里写代码。。。
+  var array = [];
+  var number;
+  for (var i = 0; i < collection.length; i++) {
+    for (var j = i+1; j < collection.length; j++) {
+      if (collection[i]>collection[j]) {
+        number=collection[i];
+        collection[i]=collection[j];
+        collection[j]=number;
+      }
+    }
+  }
+
+  var box = 0;
+  for ( i = 0; i < collection.length; i++) {
+    array.push(collection[i]);
+    if (i%3===2) {
+      box = array[i-2];
+      array[i-2] = array[i-1];
+      array[i-1] = array[i];
+      array[i] = box;
+    }
+  }
+  return array;
 }
 module.exports = rank_by_two_large_one_small;
