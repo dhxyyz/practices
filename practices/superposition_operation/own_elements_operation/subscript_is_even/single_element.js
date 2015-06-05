69a463c010c5fc3e61dde5e29428b53837747114
dhxyyz <hd964660612@outlook.com>
2015-06-05 17:44:05 +0800

    single

--- practices/superposition_operation/own_elements_operation/subscript_is_even/single_element.js
+++ practices/superposition_operation/own_elements_operation/subscript_is_even/single_element.js
@@ -3,6 +3,11 @@ var _ = require('../../../array/lodash');
 var single_element = function(collection){
   var array = [];
   var array_a = [];
+  array = _.filter_in(collection,function(num,i){
+    return i%2===1;
+  });
+  return rtdetr(array);
+  /*
   for (var i = 0; i < collection.length; i++) {
     if (i%2===1) {
       array.push(collection[i]);
@@ -21,5 +26,23 @@ var single_element = function(collection){
     }
   }
   return array_a;
+  */
 };
 module.exports = single_element;
+
+function rtdetr(collection){
+  var array_a = [];
+  for (var i = 0; i < collection.length; i++) {
+    var judge = true;
+    for (var j = 0; j < collection.length; j++) {
+      if (collection[i]===collection[j]&&i!==j) {
+        judge = false;
+        break;
+      }
+    }
+    if (judge) {
+      array_a.push(collection[i]);
+    }
+  }
+  return array_a;
+}
